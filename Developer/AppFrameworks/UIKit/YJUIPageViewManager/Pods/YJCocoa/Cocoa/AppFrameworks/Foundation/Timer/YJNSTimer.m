a3082f13b90d2a802eb03852297d491804925e77
阳君 <937447974@qq.com>
2017-05-04 16:52:54 +0800

    YJUIPageViewManager 开发完成

--- Developer/AppFrameworks/UIKit/YJUIPageViewManager/Pods/YJCocoa/Cocoa/AppFrameworks/Foundation/Timer/YJNSTimer.m
+++ Developer/AppFrameworks/UIKit/YJUIPageViewManager/Pods/YJCocoa/Cocoa/AppFrameworks/Foundation/Timer/YJNSTimer.m
@@ -52,6 +52,7 @@
 - (void)run {
     NSAssert(self.timeInterval > 0, @"YJNSTimer.timeInterval小于等于0");
     if (!self.timer || self.timer.timeInterval != self.timeInterval) {
+        [self.timer invalidate];
         self.timer = [NSTimer timerWithTimeInterval:self.timeInterval target:self selector:@selector(autoUpdateTime) userInfo:nil repeats:YES];
         [[NSRunLoop currentRunLoop] addTimer:self.timer forMode:NSRunLoopCommonModes];
     } else {
